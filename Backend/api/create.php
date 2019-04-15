e9dc774c79ea25660c6e88787d35de702505ac59
Faktra <33802789+Faktra@users.noreply.github.com>
2019-04-15 10:32:05 +0200

    .

--- Backend/api/create.php
+++ Backend/api/create.php
@@ -1,32 +1,25 @@
 <?php
 require 'database.php';
-
 // Get the posted data.
 $postdata = file_get_contents("php://input");
-
 if(isset($postdata) && !empty($postdata))
 {
   // Extract the data.
   $request = json_decode($postdata);
-
-
   // Validate.
   if(trim($request->username) === '')
   {
     return http_response_code(400);
   }
-
   // Sanitize.
   $username = mysqli_real_escape_string($con, (string)($request->username));
   $password = mysqli_real_escape_string($con, (string)$request->password);
   $first_name = mysqli_real_escape_string($con, (string)$request->first_name);
   $last_name = mysqli_real_escape_string($con, (string)$request->last_name);
   $op5_key = mysqli_real_escape_string($con, (string)$request->op5_key);
-
-
+  $hash = password_hash($password, PASSWORD_DEFAULT);
   // Create.
-  $sql = "INSERT INTO `users`(`username`,`password`, `first_name`, `last_name`, `op5_key`) VALUES ('{$username}','{$password}','{$first_name}','{$last_name}','{$op5_key}')";
-
+  $sql = "INSERT INTO `user`(`username`,`P_hash`, `first_name`, `last_name`, `op5_key`) VALUES ('{$username}','{$hash}','{$first_name}','{$last_name}','{$op5_key}')";
   if(mysqli_query($con,$sql))
   {
     http_response_code(201);
