d6ebca32838aba627769d9258a8e944d09c0e23c
ylizneva <yulializneva@Yulias-MacBook-Air.local>
2019-05-06 10:53:41 +0200

    yulia

--- Frontend/src/app/api.service.ts
+++ Frontend/src/app/api.service.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
@@ -32,3 +33,34 @@ export class ApiService {
 
     constructor(private httpClient: HttpClient) { }
   }
+=======
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  PHP_API_SERVER = "http://127.0.0.1:8080";
+  readUser(): Observable<User[]>{
+    return this.httpClient.get<User[]>(`${this.PHP_API_SERVER}/api/read.php`);
+  }
+
+  createUser(user: User): Observable<User>{
+    return this.httpClient.post<User>(`${this.PHP_API_SERVER}/api/create.php`, user);
+  }
+
+  updateUser(user: User){
+    return this.httpClient.put<User>(`${this.PHP_API_SERVER}/api/update.php`, user);
+  }
+
+  deleteUser(id: number){
+    return this.httpClient.delete<User>(`${this.PHP_API_SERVER}/api/delete.php/?id=${id}`);
+  }
+
+    constructor(private httpClient: HttpClient) { }
+  }
+>>>>>>> ba9291adef624e1f44dada864c20f0852c45efbc
